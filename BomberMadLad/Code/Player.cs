f22bfc799f22cf97839a5e76d9d9eee696f492b0
William Persson <william.persson@live.se>
2018-11-09 14:07:44 +0100

    Move

--- BomberMadLad/Code/Player.cs
+++ BomberMadLad/Code/Player.cs
@@ -11,10 +11,9 @@ namespace BomberMadLad
         bool u = false;
 
         bool layBomb;
-
+        PlayerControl control = new PlayerControl();
         //senaste bomben som spawnats
         public BOOM latestBoom;
-
         public Player(int x, int y)
         {
             layBomb = true;
@@ -42,32 +41,33 @@ namespace BomberMadLad
             //återställ oldX + old Y
             int oldX = XPosition;
             int oldY = YPosition;
-            ConsoleKey input = new ConsoleKey();
+            //ConsoleKey input = new ConsoleKey();
 
-            //kollar efter spelarens input. OBS måste bytas ut för att inte bli turnbased eftersom readkey väntar på knapptryck.
-            if (Console.KeyAvailable)
-            {
-                input = Console.ReadKey(true).Key;
-            }
+            ////kollar efter spelarens input. OBS måste bytas ut för att inte bli turnbased eftersom readkey väntar på knapptryck.
+            //if (Console.KeyAvailable)
+            //{
+            //    input = Console.ReadKey(true).Key;
+            //}
+            //control.Update();
 
-            //movement beroende på knapptryck, xpos är två steg i taget eftersom den är två pixlar bred
-            if (input == ConsoleKey.W)
-            {
-                YPosition--;
-            }
-            if (input == ConsoleKey.S)
-            {
-                YPosition++;
-            }
-            if (input == ConsoleKey.D)
-            {
-                XPosition += 2;
-            }
-            if (input == ConsoleKey.A)
-            {
-                XPosition -= 2;
-            }
-            //om collisionCheck träffar något så står vi stilla och deletar inte något
+            ////movement beroende på knapptryck, xpos är två steg i taget eftersom den är två pixlar bred
+            //if (input == ConsoleKey.W)
+            //{
+            //    YPosition--;
+            //}
+            //if (input == ConsoleKey.S)
+            //{
+            //    YPosition++;
+            //}
+            //if (input == ConsoleKey.D)
+            //{
+            //    XPosition += 2;
+            //}
+            //if (input == ConsoleKey.A)
+            //{
+            //    XPosition -= 2;
+            //}
+            ////om collisionCheck träffar något så står vi stilla och deletar inte något
             if (!CollisionCheck(XPosition, YPosition))
             {
                 XPosition = oldX;
@@ -80,21 +80,21 @@ namespace BomberMadLad
             }
 
             //lägg bomb
-            if (input == ConsoleKey.Spacebar && layBomb)
-            {
-                latestBoom = new BOOM(XPosition, YPosition);
+            //if (input == ConsoleKey.Spacebar && layBomb)
+            //{
+            //    latestBoom = new BOOM(XPosition, YPosition);
 
-                //lägg till i gameobjects
-                Program.mygame.GameObjects.Add(latestBoom);
+            //    //lägg till i gameobjects
+            //    Program.mygame.GameObjects.Add(latestBoom);
 
 
-                layBomb = true;
+            //    layBomb = true;
 
-                //lägg till timer
-                int index = TimerClass.GetIndex(latestBoom.XPosition, latestBoom.YPosition);
+            //    //lägg till timer
+            //    int index = TimerClass.GetIndex(latestBoom.XPosition, latestBoom.YPosition);
 
-                TimerClass.AddTimer(index, 1000, 500, 10, Program.mygame.GameObjects[index].Action2);
-            }
+            //    TimerClass.AddTimer(index, 1000, 500, 10, Program.mygame.GameObjects[index].Action2);
+            //}
         }
 
         public void PlayerBoomCooldown(object o)
